f46a847bced3f66348bbe9315168cd41f1e7d696
Gavin Stexk <gavinmsteck@gmail.com>
2022-08-23 23:06:28 -0600

    init commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,6 +11,10 @@ public func configure(_ app: Application) throws {
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+//        port: Environment.get("PORT").flatMap(Int.init) {
+//            app.http.server.configuration.port = port
+//        }
+
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
